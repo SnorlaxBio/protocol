c877ed90245a9ef9b48d8efe65020928206fa535
snorlax <ceo@snorlax.bio>
2024-07-05 02:36:45 +0900

    Update protocol

--- src/snorlax/protocol.h
+++ src/snorlax/protocol.h
@@ -12,4 +12,13 @@
 
 #include <snorlax.h>
 
+#include <snorlax/user-datagram.h>
+#include <snorlax/transmission-control.h>
+#include <snorlax/internet.h>
+#include <snorlax/ethernet.h>
+#include <snorlax/address-resolution.h>
+#include <snorlax/internet/version4.h>
+#include <snorlax/internet/version6.h>
+#include <snorlax/internet/control-message.h>
+
 #endif // __SNORLAX__PROTOCOL__H__
