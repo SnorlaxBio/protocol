0a03ebe70d0c088fe59dd8657e85fb3b2b07a6a9
snorlax <ceo@snorlax.bio>
2024-07-10 16:58:23 +0900

    Add netlink protocol.

---
+++ src/snorlax/protocol/netlink.h
@@ -0,0 +1,15 @@
+/**
+ * @file        snorlax/protocol/netlink.h
+ * @brief
+ * @details
+ * 
+ * @author      snorlax <ceo@snorlax.bio>
+ * @since       July 10, 2024
+ */
+
+#ifndef   __SNORLAX__PROTOCOL_NETLINK__H__
+#define   __SNORLAX__PROTOCOL_NETLINK__H__
+
+#include <snorlax.h>
+
+#endif // __SNORLAX__PROTOCOL_NETLINK__H__
