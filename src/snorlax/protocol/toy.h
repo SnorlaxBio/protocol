3a2d13f4903caed375f2c60e23dd4bd4d91d55ab
snorlax <ceo@snorlax.bio>
2024-07-20 11:15:12 +0900

    Update Daily

---
+++ src/snorlax/protocol/toy.h
@@ -0,0 +1,17 @@
+/**
+ * @file        snorlax/protocol/toy.h
+ * @brief
+ * @details
+ * 
+ * @author      snorlax <ceo@snorlax.bio>
+ * @since       July 19, 2024
+ */
+
+#ifndef   __SNORLAX__PROTOCOL_TOY__H__
+#define   __SNORLAX__PROTOCOL_TOY__H__
+
+#include <snorlax.h>
+
+// #define snorlax_protocol_
+
+#endif // __SNORLAX__PROTOCOL_TOY__H__
