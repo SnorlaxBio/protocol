1f32e5c2baa42f4bd49bcbfe34f3e52323383f53
snorlax <ceo@snorlax.bio>
2024-07-26 18:32:01 +0900

    update protocol number header file.

---
+++ src/snorlax/protocol/number.h
@@ -0,0 +1,15 @@
+/**
+ * @file        snorlax/protocol/number.h
+ * @brief
+ * @details
+ * 
+ * @author      snorlax <ceo@snorlax.bio>
+ * @since       July 26, 2024
+ */
+
+#ifndef   __SNORLAX__PROTOCOL_NUMBER__H__
+#define   __SNORLAX__PROTOCOL_NUMBER__H__
+
+#define protocol_number_exterior_gateway            8
+
+#endif // __SNORLAX__PROTOCOL_NUMBER__H__
